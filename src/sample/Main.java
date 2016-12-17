5e3ce80074710eb7eddc0374c1d2a05dabbf4d48
Pastushkov Roman <turbopollux@rambler.ru>
2016-12-18 02:54:04 +0300

    Please enter the commit message for your changes. Lines starting

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,10 +11,10 @@ import java.util.Scanner;
 public class Main extends Application {
     public static int N;
     static int deltay=15;
-    static int deltax=20;
+    static int deltax=25;
     @Override
     public void start(Stage primaryStage) throws Exception{
-        primaryStage.setTitle("Drawing Operations Test");
+        primaryStage.setTitle("SimplePyramyde");
         Group root = new Group();
         Canvas canvas = new Canvas(maxposition(N)*deltax, N*deltay);
         GraphicsContext gc = canvas.getGraphicsContext2D();
@@ -24,17 +24,17 @@ public class Main extends Application {
         primaryStage.show();
     }
 
-    private static void drawShapes(GraphicsContext gc) {
+    private  void drawShapes(GraphicsContext gc) {
         int initial_x =0;
         int initial_y =0;
         int start_value = 1;
         int const_shift = shift_x(N);
-        int textxoffset=2;
-        int textyoffset=8;
-
-        gc.setFont(new Font("arial", 8));
+        int fontsize =10;
+        int textxoffset=3;
+        int textyoffset=10;
+        gc.setFont(new Font("arial", fontsize));
         for (int i = 1; i<=N; i++){
-                for (int j = 1; j<=(maxposition(N)-maxposition(i))/2; j++) {
+            for (int j = 1; j<=(maxposition(N)-maxposition(i))/2; j++) {
                 gc.setFill(Color.LIGHTSLATEGRAY);
                 gc.fillRect(initial_x, initial_y, const_shift, deltay);
                 initial_x = initial_x + const_shift;
@@ -58,15 +58,16 @@ public class Main extends Application {
             initial_y=initial_y+deltay;
             initial_x =0;
         }
-        }
+    }
     public static void main(String[] args) {
-       System.out.print("enter N ");
+        System.out.print("enter N ");
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
         System.out.print(N);
         launch(args);
     }
-    static boolean yellow_back(int A)
+
+    boolean yellow_back(int A)
     {
         if (A == 1) return false;
         for ( int i = 2; i<A; i++)
@@ -76,16 +77,16 @@ public class Main extends Application {
         return true;
     }
 
-    static int shift_x(int N) {//shift (width 1 cell)
+    int shift_x(int N) {//shift (width 1 cell)
         return expo_counter(maxposition(N))*deltax/2;
     }
 
-    static int maxposition(int A)
+    int maxposition(int A)
     {
         return (A * 2 - 1);
     }
 
-    static int expo_counter(int A) {
+    int expo_counter(int A) {
         int result = 1;
         while (true) {
             A = Math.abs(A / 10);
